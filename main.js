0f6119f4cb85b71c06ceb2e60f2c314365241734
Linnea <linneaj95@hotmail.com>
2018-10-22 14:02:11 +0200

    Added eventlisteners to all the buttons

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
       // get and create the html-elements
       var gameContainer = document.getElementById("content");
       var list = document.createElement("ul");
+      list.setAttribute("id", "list");
 
       for (var i = 0; i < res.results.length; i++) {
         var q = res.results[i];
@@ -18,17 +19,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
         // print out the question
         var question = q.question;
-        qItem.innerHTML = question + "<br>" + q.correct_answer + "<br><br>";
+        qItem.innerHTML = question + "<br>";
 
+        // put all the alternatives in one array
         var alteratives = q.incorrect_answers;
         alteratives.push(q.correct_answer);
 
-        console.log(alteratives)
-
+        // create buttons for answers
         for (var j = 0; j < alteratives.length; j++) {
           var button = document.createElement("input");
           button.type = "submit";
           button.value = alteratives[j];
+          button.setAttribute("class", "button")
           if (button.value == q.correct_answer) {
             button.setAttribute("id", "correct")
           }
@@ -42,6 +44,17 @@ document.addEventListener("DOMContentLoaded", function() {
       // add the list to the div in the html
       gameContainer.appendChild(list);
 
-    });
-
+      addEvents();
+  });
 });
+
+
+// add eventlisteners on all the buttons
+function addEvents() {
+  var buttons = document.getElementsByTagName("input");
+  for (var x = 0; x < buttons.length; x++) {
+    buttons[x].addEventListener("click", function() {
+      console.log(this.id);
+    });
+  }
+}
